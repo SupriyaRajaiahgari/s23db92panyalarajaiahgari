ba412b9010e8951b6d954cda4cc7241e973d8d10
SupriyaRajaiahgari <S566492@nwmissouri.edu>
2023-11-10 10:38:56 -0600

    mpodified

--- controllers/books.js
+++ controllers/books.js
@@ -4,9 +4,17 @@ exports.books_list = function(req, res) {
 res.send('NOT IMPLEMENTED: books list');
 };
 // for a specific books.
-exports.books_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: books detail: ' + req.params.id);
-};
+// for a specific books.
+exports.books_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await books.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 // Handle books create on POST.
 exports.books_create_post = async function(req, res) {
     console.log(req.body)
@@ -14,7 +22,7 @@ let document = new books();
 // We are looking for a body, since POST does not have query parameters.
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
-// {"books_type":"goat", "cost":12, "size":"large"}
+// {"books_name":"goat", "cost":12, "size":"large"}
 document.books_name = req.body.books_name;
 document.books_author = req.body.books_author;
 document.books_cost = req.body.books_cost;
@@ -32,8 +40,25 @@ exports.books_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: books delete DELETE ' + req.params.id);
 };
 // Handle books update form on PUT.
-exports.books_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: books update PUT' + req.params.id);
+//Handle books update form on PUT.
+exports.books_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await books.findById( req.params.id)
+// Do updates of properties
+if(req.body.books_name)
+toUpdate.books_name = req.body.books_name;
+if(req.body.books_author) toUpdate.books_author = req.body.books_author;
+if(req.body.books_cost) toUpdate.books_cost= req.body.books_cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 
 exports.books_list = async function(req, res) {
