09b24150017f5d2426822df4401c69716c765f6d
SupriyaRajaiahgari <S566492@nwmissouri.edu>
2023-11-28 17:17:28 -0600

    modified

--- models/books.js
+++ models/books.js
@@ -1,8 +1,17 @@
 //const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const booksSchema = mongoose.Schema({
-books_name: String,
+books_name: {
+    type: String,
+    required: true,
+    maxLength: [8]
+ },
 books_author: String,
-books_cost:Number,
+books_cost:{
+    type: Number,
+    required: true,
+    min: 100,
+    max: 1000,
+  },
 })
 module.exports = mongoose.model("books",booksSchema)
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const books_controllers = require('../controllers/books');
+const passport = require('passport');
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,16 +18,20 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET detail books page */
-router.get('/detail', books_controllers.books_view_one_Page);
+router.get('/detail',secured, books_controllers.books_view_one_Page);
 
 /* GET create books page */
-router.get('/create', books_controllers.books_create_Page);
+router.get('/create',secured, books_controllers.books_create_Page);
 
 /* GET create update page */
-router.get('/update', books_controllers.books_update_Page);
+router.get('/update',secured, books_controllers.books_update_Page);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+  });
 
 /* GET delete books page */
-router.get('/delete', books_controllers.books_delete_Page);
+router.get('/delete',secured, books_controllers.books_delete_Page);
 
 
 
